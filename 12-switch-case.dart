63e7153ca5779c65ecd0f7ae59f7647118a924ed
Bruna R Silva <162511835+BrunaSilva97@users.noreply.github.com>
2026-08-28 21:34:44 -0300

    Create 12-switch-case.dart

---
+++ 12-switch-case.dart
@@ -0,0 +1,11 @@
+void main() {
+  int opcao = 2;
+  
+  switch(opcao){
+    case 1: print ("Cadastrar"); break;
+    case 2: print ("Listar"); break;
+    case 3: print ("Sair"); break;
+    default: print ("Opção inválida");
+  }
+
+}
