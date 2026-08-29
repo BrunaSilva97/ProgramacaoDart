4fcc3e540808b5b02cb8f1c19e705f0f9e15b1ad
Bruna R Silva <162511835+BrunaSilva97@users.noreply.github.com>
2026-08-28 21:21:58 -0300

    Create 09-interpolacao.dart

---
+++ 09-interpolacao.dart
@@ -0,0 +1,10 @@
+void main() {
+  String nome = 'Ariel';
+  double nota = 10;
+  
+  print ('Aluno: $nome');
+  print ('Nota final: ${nota.toStringAsFixed(1)}');
+
+}
+
+Interpolação: Junção de texto e número (mas o toString muda o número para texto e adiciona uma casa decimal)
