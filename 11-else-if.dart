fab65bc772d93f8fbc45caea6661f54f21f6e699
Bruna R Silva <162511835+BrunaSilva97@users.noreply.github.com>
2026-08-28 21:29:09 -0300

    Create 11-else-if.dart

---
+++ 11-else-if.dart
@@ -0,0 +1,12 @@
+void main() {
+  int idade = 17;
+  
+  if(idade < 16){
+    print("Não vota");
+  }else if(idade <18 || idade >= 70){
+    print("Voto facultativo");
+  }else{
+    print("Voto é obrigatório");
+  }
+
+}
