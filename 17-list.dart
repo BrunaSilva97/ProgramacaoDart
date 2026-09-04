0c15effd475a61be4206d4859a602f14f6df8819
Bruna R Silva <162511835+BrunaSilva97@users.noreply.github.com>
2026-09-04 20:20:35 -0300

    Create 17-list.dart

---
+++ 17-list.dart
@@ -0,0 +1,9 @@
+void main() {
+  List<String> alunos = ['Adryan','Alan','Ariel','Amanda'];
+  print(alunos);
+  alunos.add('Bruna');
+  print(alunos);
+  alunos.remove('Adryan');
+  print(alunos);
+  print(alunos[0]);
+}
