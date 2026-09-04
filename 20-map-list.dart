3df085261a65cae59218b00e4118fcdf9a4129ef
Bruna R Silva <162511835+BrunaSilva97@users.noreply.github.com>
2026-09-04 20:44:02 -0300

    Create 20-map-list.dart

---
+++ 20-map-list.dart
@@ -0,0 +1,7 @@
+void main() {
+  final numeros = [1, 2, 3, 4, 5, 6, 7, 8];
+  final dobrados = numeros.map((n) => n * 2).toList();
+  print(dobrados);
+}
+
+//.map recria a lista de acordo com o solicitado
