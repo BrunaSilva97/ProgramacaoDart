49b5083a004588befcb640c8287b2492b33de007
Bruna R Silva <162511835+BrunaSilva97@users.noreply.github.com>
2026-09-04 20:49:01 -0300

    Update 20-map-list.dart

--- 20-map-list.dart
+++ 20-map-list.dart
@@ -2,6 +2,12 @@ void main() {
   final numeros = [1, 2, 3, 4, 5, 6, 7, 8];
   final dobrados = numeros.map((n) => n * 2).toList();
   print(dobrados);
+  
+  final pares = numeros.where((n) => n.isEven).toList();
+  print(pares);
+  
+  final primeiroMaiorQue = numeros.firstWhere((n) => n >3);
+  print(primeiroMaiorQue);
 }
 
 //.map recria a lista de acordo com o solicitado
