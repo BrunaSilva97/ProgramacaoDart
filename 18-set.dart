1244bd3761f74b55ef25224fc5918b9cbbd1fc5d
Bruna R Silva <162511835+BrunaSilva97@users.noreply.github.com>
2026-09-04 20:24:56 -0300

    Create 18-set.dart

---
+++ 18-set.dart
@@ -0,0 +1,7 @@
+void main() {
+  Set<String> cursos = {'Dart','Flutter','Dart'};
+  cursos.add('Node.js');
+  print(cursos);
+}
+
+//Set trabalha com elementos duplicados, vai remover o que estiver repetido
